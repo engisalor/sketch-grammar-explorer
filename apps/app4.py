8859924192068630b2e496de565fd40394d7ca94
engisalor <50170623+engisalor@users.noreply.github.com>
2021-02-11 10:32:20 -0700

    add ws_freqs_api, rename

--- apps/app4.py
+++ apps/app4.py
@@ -2,41 +2,14 @@ import dash
 import dash_html_components as html
 import dash_table
 import pandas as pd
-import numpy as np
+#import numpy as np
 import re
 from app import app
 
 #### GET DATA
 
 # load
-ttypes = np.load('data/PR/ttypes.npy',allow_pickle='TRUE').item()
-
-# create df and fill with blocks
-freqs1 = [ttypes["Blocks"][x]["Items"] for x in range(0, len(ttypes["Blocks"]))]
-temp = pd.DataFrame()
-for x in range(0, len(freqs1)):
-    temp = temp.append(pd.DataFrame.from_dict(freqs1[x]))
-
-# add second index
-temp.reset_index(inplace=True)
-
-# add text type column
-ls = []
-for x in range(0, len(ttypes['Blocks'])):
-    ls = ls + [(ttypes['Blocks'][x]['Head'][0]['n'])] * len(ttypes["Blocks"][x]["Items"])
-temp["ttype"] = ls
-
-# add item column
-temp["item"] = range(0,len(temp))
-for x in range(0,len(temp)):
-    temp["item"][x] =  re.search(r" '.*'}",str(temp["Word"][x])).group(0)
-    temp["item"][x] = temp["item"][x][2:-2]
-
-# add index1
-temp["all"] = range(0, len(temp))
-
-# get relevant columns to graph
-temp = temp.filter(["all", "index", "ttype", "item", "frq", "norm", "rel", "fpm"], axis=1)
+temp = pd.read_csv('data/PR/ttypes.csv')
 
 #### LAYOUT
 
---
+++ ws_freqs_api.py
@@ -0,0 +1,78 @@
+import requests
+import numpy as np
+import pandas as pd
+import time
+import re
+
+#### 
+# this script retrives word sketch frequencies for: 
+# * a specific relation (is a type of..., caused by, etc.) as defined in a sketch grammar
+# * using a list of corpus text types (Editor, Domain, etc.)
+# * and all of their entries (Academic, Government, etc.)
+# it's not designed for text types with thousands of entries (author, key words, etc.) 
+####
+
+# TODO test fmaxitems
+# TODO run API calls and visualize results in app
+
+# get login credentials
+with open("auth_api.txt") as f:
+    LOGIN = dict(x.rstrip().split(":") for x in f)
+
+# get corpus text types
+df = pd.read_csv('data/PR/ttypes.csv')
+# corp_info = np.load("corp_info.npy", allow_pickle="TRUE").item() # secondary method
+
+# get grammar
+with open("grammar.txt") as f:
+    lines = [line.rstrip() for line in f]
+lines = lines[lines.index("### Pilar's relations start here") :]
+
+# get word sketch names in grammar
+wslist = []
+for x in range(0, len(lines)):
+    if "%" in lines[x]:
+        wslist = wslist + lines[x].split("/")
+
+# prep word sketch names for API query
+wslist = ['"' + re.sub('.*" ', ".*", w) + '"' for w in wslist]
+
+
+# make requests
+for x,y in df.filter(["ttype", "item"], axis=1).to_numpy(): # for all ttypes
+    # define query
+    wstype = wslist[1] # pick one wstype here
+    ttypestr = 'within <doc (' + x.lower() + '="' + y + '") />'
+    b = '[ws(".*-n",' + wstype + ',".*-n")]' + ttypestr
+    # b = '[ws(".*-n",".*type of.*",".*-n")]' # a generic search
+
+    # make url
+    base_url = "https://api.sketchengine.eu/bonito/run.cgi/"
+    query_type = "freqs?"
+    cql_query = b
+    data = {
+        "q": "q" + cql_query,
+        "fcrit": "lemma/e 0~0>0",
+        "fmaxitems": "100",
+        "corpname": "preloaded/ecolexicon_en",
+        "username": LOGIN["username"],
+        "api_key": LOGIN["api_key"],
+        "async": "0",
+        "format": "json",
+    }
+    print("... making request " + wstype + " " + x + "_" + y)
+    d = requests.get(base_url + query_type, params=data).json()
+
+    # save
+    filename = ''.join(filter(str.isalpha, wstype)) + "_" + ''.join(filter(str.isalpha, x)) + "_" + ''.join(filter(str.isalpha, y))
+    np.save("data/ws/ws_freqs_" + filename, d)
+    # sleep
+    time.sleep(4)
+
+# Sketch Engine usage policy:
+# <50 requests, no waiting
+# <900 requests, 4 seconds per query
+# 2000< requests, 44 seconds per query
+
+# to load a file
+# freqs = np.load('freqs/freqs145.npy',allow_pickle='TRUE').item()
