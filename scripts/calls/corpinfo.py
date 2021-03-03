f3e2bfb8f3bc1365137da5ff7078bc3652ae8cad
engisalor <50170623+engisalor@users.noreply.github.com>
2021-03-03 17:02:46 +0100

    add corpinfo draft lines for data manipulation

--- scripts/calls/corpinfo.py
+++ scripts/calls/corpinfo.py
@@ -4,7 +4,7 @@ import json
 from pathlib import Path
 
 ###
-# template for corpus info calls (necessary for some other call types)
+# template for corpus info calls (prerequisite for some other call types)
 ###
 
 def corpinfo(save = False, corpus = "preloaded/ecolexicon_en"):
@@ -49,5 +49,21 @@ def corpinfo(save = False, corpus = "preloaded/ecolexicon_en"):
 
     # return
     else:
+
+        # TODO extract corpus data for visualization/piping to other calls
+        # # get corpus structures
+        # dt = {}
+        # for y in range(len(d["structs"])):
+        #     for x in range(len(d["structs"][y][2])):
+        #         dt[d["structs"][y][2][x][0]] = d["structs"][y][2][x][2]
+        # # set text types
+        # if d["request"]["corpname"] == "user/PilarLeon/hejuly2019_backup":
+        #     for x in ["doc.id","doc.filename","doc.wordcount"]: 
+        #         dt.pop(x)
+        # if d["request"]["corpname"] == "preloaded/ecolexicon_en":
+        #     for x in ["doc.author","doc.keywords","doc.title","doc.wordcount"]: 
+        #         dt.pop(x)
+        # ttypes = list(dt.keys())    
+
         print("DONE\n")
         return d
