7a31ca6ca719bd44350176191a0f2dd27b378ec8
engisalor <50170623+engisalor@users.noreply.github.com>
2021-03-04 23:46:29 +0100

    add callshelper, del multicall, add getwstypes

--- scripts/callshelper.py
+++ scripts/callshelper.py
@@ -1,3 +1,5 @@
+import re
+from pathlib import Path
 import scripts.calls as calls
 
 ###
@@ -22,3 +24,24 @@ def multicall(queries):
         calls.wait(len(queries[1]))
     print("DONE\n")
     return results
+
+###
+# get list of different kinds of word sketches in a grammar
+###
+
+def getwstypes(grammar = "grammar.txt"):
+    # set data paths
+    data_folder = Path("")
+    fgrammar = data_folder / grammar
+    # get grammar file
+    with open(fgrammar) as f:
+        lines = [line.rstrip() for line in f]
+    lines = lines[lines.index("### Pilar's relations start here") :]
+    # make list of word sketch types
+    wslist = []
+    for x in range(0, len(lines)):
+        if "%" in lines[x]:
+            wslist = wslist + lines[x].split("/")
+    # prep word sketch list for API usage
+    wslist = ['"' + re.sub('.*" ', ".*", w) + '"' for w in wslist]
+    return wslist
