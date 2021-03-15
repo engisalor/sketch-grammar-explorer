672432d923b176759c7cd3ce00311de980e8dbb1
engisalor <50170623+engisalor@users.noreply.github.com>
2021-03-15 22:52:32 +0100

    improve class methods, add label()

--- scripts/classes.py
+++ scripts/classes.py
@@ -26,27 +26,25 @@ class Call:
             lines = [x for x in clist.splitlines() if x]
             # separate calls from labels
             calls = [ast.literal_eval("{" + lines[x] + "}") for x in range(len(lines)) if lines[x][0] != "#"]
-            # copy params * len(clist)
-            formatted = [self.params]*(len(calls)+1)
+            # copy params by clist len
+            formatted = [self.params]*(len(calls))
             # set unique parameters for c in clist
             for x in range(len(calls)):
                 formatted[x] = {**formatted[x], **calls[x]}
             # set q
             formatted = [self.setq(params) for params in formatted]
-            # remove identical calls
-            return self.uniquec(formatted)
+            # remove identical calls and NAs
+            return [x for x in self.uniquec(formatted) if x]
 
     def uniquec(self,formatted):
         # make immutable
         immutable = [json.dumps(x, sort_keys=True) for x in formatted]
-        # replace repeats with None 
-        for y in range(len(immutable)):
-            reps = []
+        # replace repeats with None starting from end
+        for y in reversed(range(len(immutable))):
             if immutable.count(immutable[y]) > 1:
                 immutable[y] = "None"
         # return to dicts
-        immutable = [ast.literal_eval(x) for x in immutable]
-        return immutable
+        return [ast.literal_eval(x) for x in immutable]
 
     def setq(self,params):
         temp = params.copy()
@@ -55,11 +53,42 @@ class Call:
             r = "r" + str(params["pagesize"])
             q = [self.settings["qattr"] + q, r]
             temp["q"] = q
-            return temp
         else:
-            temp["q"] = q
             q = [self.settings["qattr"] + q]
-            return temp
+            temp["q"] = q
+        return temp
+
+    def label(self):
+        # get lines
+        clist = re.sub(' +', '', self.clist)
+        lines = [x for x in clist.splitlines() if x]
+        # make calls comparable strings
+        for x in range(len(lines)):
+            if lines[x][0] != "#":
+                lines[x] = ast.literal_eval("{" + lines[x] + "}")
+                lines[x] = json.dumps(lines[x], sort_keys=True)
+        lines = [x for x in Call.uniquec(None,lines) if x]
+        # add labels
+        for x in range(len(lines)):
+            # single labels
+            if lines[x][0] == "#" and lines[x][1] != "#":
+                lines[x+1] = lines[x].strip("# ")
+            # group labels
+            if lines[x][:3] == "###":
+                stop = [lines[x+1:].index(s) for s in lines[x+1:] if s[0] == "#"]
+                # if a label exists after
+                if len(stop) != 0:
+                    for n in range(1,stop[0]+1):
+                        lines[x+n] = lines[x].strip("# ")
+                # if no labels after
+                if len(stop) == 0:
+                    for n in range(x+1,len(lines)):
+                        lines[n] = lines[x].strip("# ")
+            # if no label
+            if lines[x][0] == "{":
+                lines[x] = None
+        # drop old
+        return [x for x in lines if x is None or x[0] != "#"]
 
 class view(Call):
     """
@@ -85,17 +114,17 @@ class view(Call):
         self.clist = clist
         self.settings = settings
         self.formatted = self.formatc()
+        self.labels = self.label()
 
 clist = """
 "q": ''' "car" '''
-### GROUP
 "q": ''' "water" '''
 # SINGLE
 "q": ''' "pie" '''
 "q": ''' "water" '''
-# """
+"""
+
 p = {'q': '"water"','refs': 'doc,s', 'corpname': "preloaded/ecolexicon_en", 'viewmode': 'sen', 'pagesize': 100, 'fromp': 1}
 s = {"qattr": "alemma,", "randomize": True}
 
 c = view(p,s,clist)
-c.formatted
