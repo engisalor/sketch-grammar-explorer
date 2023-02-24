9bb7f23f88afd3a807e8547a59c29faca80046a2
engisalor <50170623+engisalor@users.noreply.github.com>
2023-02-24 22:08:28 +0100

    fix: add util module

---
+++ sgex/util.py
@@ -0,0 +1,28 @@
+"""Utility functions."""
+import pandas as pd
+
+
+def list_of_dict(ls: list) -> dict:
+    """Recursively converts a list of dicts to a dict of lists.
+
+    Notes:
+        Returns objects as-is if they're not lists or dicts."""
+
+    def _flatten(ls):
+        if not isinstance(ls, list):
+            return ls
+        else:
+            if dict not in [type(x) for x in ls]:
+                return ls
+            # for [dict, dict, nan] objects
+            else:
+                # convert nan to empty dict
+                ls = [x if isinstance(x, dict) else {} for x in ls]
+                # convert list of dicts to dict of lists
+                dt = pd.DataFrame(ls).to_dict(orient="list")
+                # continue with recursion if needed
+                for k, v in dt.items():
+                    dt[k] = _flatten(v)
+                return dt
+
+    return _flatten(ls)
