b0288f2378819523aabb89cbf2a5607d4f3e5bdf
engisalor <50170623+engisalor@users.noreply.github.com>
2023-02-25 12:20:59 +0100

    fix(io): add export_content

--- sgex/io.py
+++ sgex/io.py
@@ -1,6 +1,7 @@
 """Read/write functions."""
 import json
 import pathlib
+from urllib.parse import parse_qs, urlparse
 
 import pandas as pd
 import yaml
@@ -51,36 +52,68 @@ def write_json(file: str, dt: dict, prompt: bool = True) -> None:
             json.dump(dt, f, indent=2, ensure_ascii=False)
 
 
-def data_to_csv(file: str, response: Response):
-    with open(file, "w", encoding="utf-8") as f:
+def to_csv(response: Response, filename: str = "content"):
+    with open(f"{filename}.csv", "w", encoding="utf-8") as f:
         f.write(response.text)
 
 
-def data_to_json(file: str, response: Response):
-    with open(file, "w", encoding="utf-8") as f:
+def to_json(response: Response, filename: str = "content"):
+    with open(f"{filename}.json", "w", encoding="utf-8") as f:
         json.dump(response.json(), f, ensure_ascii=False, indent=1)
 
 
-def data_to_txt(file: str, response: Response):
-    with open(file, "w", encoding="utf-8") as f:
+def to_txt(response: Response, filename: str = "content"):
+    with open(f"{filename}.txt", "w", encoding="utf-8") as f:
         f.write(response.text)
 
 
-def data_to_xlsx(file: str, response: Response):
+def to_xlsx(response: Response, filename: str = "content"):
     xlsx = pd.read_excel(response.content, header=None)
-    xlsx.to_excel(file, header=False, index=False)
+    xlsx.to_excel(f"{filename}.xlsx", header=False, index=False)
 
 
-def data_to_xml(file: str, response: Response):
+def to_xml(response: Response, filename: str = "content"):
     from defusedxml import ElementTree as etree
 
     xml = etree.fromstring(response.content)
-    with open(file, "wb") as f:
+    with open(f"{filename}.xml", "wb") as f:
         f.write(
             etree.tostring(
                 xml,
                 encoding="UTF-8",
                 xml_declaration=True,
-                pretty_print=True,
             )
         )
+
+
+def export_content(response: Response, filename: str = "content"):
+    """Exports the content of a Response to its native format.
+
+    Args:
+        response: A ``Response`` object.
+        filename: Name for the generated file (the extension is set automatically).
+
+    Notes:
+        Output formats: ``["json", "xml", "xls", "csv", "txt"]``.
+
+        The availability of these formats depends on the API call (freqs, view, etc.)
+        and the server being used. Only JSON is universally available.
+
+    Raises:
+        If an error is raised, check ``Response.content`` for any internal SkE errors.
+        Try making the same API call with ``"format": "json"``.
+    """
+    query = parse_qs(urlparse(response.url).query)
+    _format = query.get("format", "json")[0]
+    if _format == "json":
+        to_json(response, filename)
+    elif _format == "xml":
+        to_xml(response, filename)
+    elif _format == "csv":
+        to_csv(response, filename)
+    elif _format == "txt":
+        to_txt(response, filename)
+    elif _format in ["xls", "xlsx"]:
+        to_xlsx(response, filename)
+    else:
+        raise ValueError(f"unable to export to {_format}: {response.content}")
