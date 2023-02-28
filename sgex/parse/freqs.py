69cbe81a1f5c02e9c8c6240267a9f26104ae9107
engisalor <50170623+engisalor@users.noreply.github.com>
2023-02-28 16:28:54 +0100

    refactor(parse): freqs module, docstrings

--- sgex/parse/freqs.py
+++ sgex/parse/freqs.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from requests import Response
 
-from sgex.util import list_of_dict
+from sgex.util import flatten_ls_of_dt
 
 
 def clean_items(items, item_keys=["Word", "frq", "rel", "fpm", "reltt"]) -> list:
@@ -11,7 +11,7 @@ def clean_items(items, item_keys=["Word", "frq", "rel", "fpm", "reltt"]) -> list
         b = []
         for i in block:
             dt = {k: v for k, v in i.items() if k in item_keys}
-            words = list_of_dict(dt["Word"])
+            words = flatten_ls_of_dt(dt["Word"])
             dt["value"] = "|".join(words["n"])
             del dt["Word"]
             b.append(dt)
@@ -28,7 +28,24 @@ def clean_heads(heads) -> list:
 
 
 def freqs_json(response: Response) -> pd.DataFrame:
-    """Converts a single-/multi-block freqs JSON response to a DataFrame."""
+    """Converts a single-/multi-block freqs JSON response to a DataFrame.
+
+    Args:
+        response: Response object.
+
+    Example:
+        >>> call = Freqs({
+            "corpname": "susanne", "q": 'alemma,"day"', "fcrit": "doc.file 0"
+            })
+        >>> p = Package(call, "noske", default)
+        >>> p.send_requests()
+        >>> df = freqs.freqs_json(p.responses[0])
+        >>> df.iloc[0]
+        frq                     8
+        rel              426.2205
+        reltt         3286.770748
+        ...
+    """
     json = response.json()
     # extract data from response
     blocks = json.get("Blocks", [])
@@ -44,12 +61,12 @@ def freqs_json(response: Response) -> pd.DataFrame:
         # convert to DataFrame
         df = pd.DataFrame.from_records([x for y in items for x in y])
         # get specific values
-        df["arg"] = json.get("Desc", [])[0].get("arg", {})
-        df["nicearg"] = json.get("Desc", [])[0].get("nicearg", {})
-        df["corpname"] = json.get("request", {}).get("corpname", {})
+        df["arg"] = json.get("Desc", [])[0].get("arg", None)
+        df["nicearg"] = json.get("Desc", [])[0].get("nicearg", None)
+        df["corpname"] = json.get("request", {}).get("corpname", None)
         # NOTE API uses "rel" in "Desc" to refer to a query's
         # fpm in the whole corpus as shown in the user interface
-        df["total_fpm"] = json.get("Desc", [])[0].get("rel", {})
-        df["total_size"] = json.get("Desc", [])[0].get("size", {})
-        df["fmaxitems"] = json.get("request", {}).get("fmaxitems", {})
+        df["total_fpm"] = json.get("Desc", [])[0].get("rel", None)
+        df["total_frq"] = json.get("Desc", [])[0].get("size", None)
+        df["fmaxitems"] = json.get("request", {}).get("fmaxitems", None)
         return df
