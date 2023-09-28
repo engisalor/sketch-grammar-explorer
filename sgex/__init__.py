47143e652dd701d9034711318c7aeca048b129ad
engisalor <50170623+engisalor@users.noreply.github.com>
2023-09-28 17:34:44 +0200

    fix: upate package init, gh workflow

--- sgex/__init__.py
+++ sgex/__init__.py
@@ -1,21 +1,2 @@
-"""An API wrapper for the Sketch Engine language corpus software."""
-from sgex import _call, call, config, io, parse, util
-from sgex._version import __version__
-from sgex.call.package import Package
-from sgex.call.query import simple_query
-from sgex.call.type import (
-    AttrVals,
-    Call,
-    Collx,
-    CorpInfo,
-    ExtractKeywords,
-    Freqml,
-    Freqs,
-    Freqtt,
-    Subcorp,
-    Thes,
-    View,
-    Wordlist,
-    Wsdiff,
-    Wsketch,
-)
+"""A controller for the Sketch Engine API."""
+__version__ = "0.6.3"  # x-release-please-version
