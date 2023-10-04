5ebbaba7924942a332c4f7868321860e7dcc21c0
engisalor <50170623+engisalor@users.noreply.github.com>
2023-10-04 10:11:37 +0200

    fix: various improvements to job module
    - simplify args
    - refactor call sending methods
    - bug fixes

--- sgex/job.py
+++ sgex/job.py
@@ -7,11 +7,12 @@ import os
 import shutil
 import sys
 from pathlib import Path
-from time import perf_counter, sleep
+from time import perf_counter
 
 import aiofiles
 import aiohttp
 import pandas as pd
+from multidict import MultiDict
 from yarl import URL
 
 from sgex import call as call
@@ -31,15 +32,21 @@ class CachedResponse:
     def redact_json(dt: dict) -> str:
         """Removes API credentials from a dict."""
         if dt.get("request"):
-            dt["request"].pop("username", None)
-            dt["request"].pop("api_key", None)
+            _ = dt["request"].pop("api_key", None)
+            _ = dt["request"].pop("username", None)
         return dt
 
     @staticmethod
     def redact_url(url: URL) -> URL:
         """Removes API credentials from a yarl url."""
-        dt = {k: v for k, v in url.query.items() if k not in ["username", "api_key"]}
+        dt = MultiDict(url.query)
+        _ = dt.popall("api_key", None)
+        _ = dt.popall("username", None)
         return url.with_query(dt)
+
+    def print_ske_error(self, meta):
+        if meta["ske_error"] and meta["ske_error"] != "unimplemented":
+            print(f'... {meta["ske_error"]}')
 
     async def set(self, response: aiohttp.ClientResponse):
         """Parses a Response and saves to cache."""
@@ -52,9 +59,10 @@ class CachedResponse:
         if "application/json" in response.headers.get("Content-Type"):
             j = await response.json()
             meta["ske_error"] = j.get("error", None)
+            self.print_ske_error(meta)
             self.text = json.dumps(self.redact_json(j), indent=2)
         else:
-            meta["ske_error"] = "not implemented"
+            meta["ske_error"] = "unimplemented"
             self.text = await response.text()
             self.text = self.text.lstrip("\ufeff")
         for k, v in meta.items():
@@ -71,6 +79,7 @@ class CachedResponse:
             dt |= {"text": f.read()}
         for k, v in dt.items():
             setattr(self, k, v)
+        self.print_ske_error(dt)
 
     def __init__(self, file_meta: Path, file_text: Path) -> None:
         self.file_meta = file_meta
@@ -103,11 +112,7 @@ class Job:
     def assemble_calls(self):
         """Converts parameters from input sources to `call.Call` objects."""
         self.calls = [
-            getattr(call, self.call_type)(
-                {"corpname": self.corpus, "format": self.format} | p
-            )
-            for p in self.calls
-            if p
+            getattr(call, self.call_type)(p) for p in self.calls if isinstance(p, dict)
         ]
         self.calls_len = len(self.calls)
 
@@ -128,48 +133,63 @@ class Job:
             waits.append(max([float(k) for k in self.wait_dict.keys()]))
         self.wait = min(waits)
 
-    async def send_call(self, call, session, **kwargs) -> None:
+    async def send_call(
+        self, call: call.Call, session: aiohttp.ClientSession, **kwargs
+    ) -> None:
         """Gets API data, whether it's cached locally or requires an API request."""
         file = Path(self.cache_dir) / Path(call.to_hash())
+        _format = call.params.get("format", "json")
         call.response = CachedResponse(
-            file.with_suffix(".meta.json"), file.with_suffix(f".{self.format}")
+            file.with_suffix(".meta.json"), file.with_suffix(f".{_format}")
         )
         if call.response.is_cached:
             call.response.get()
         else:
+            if self.username and self.api_key:
+                params = call.params | {
+                    "username": self.username,
+                    "api_key": self.api_key,
+                }
+            else:
+                params = call.params
+            if self.wait and not self.thread:
+                self.wait_current += self.wait
+                await asyncio.sleep(self.wait_current)
             async with session.get(
                 url=self.server.rstrip("/") + "/" + call.type,
-                params=call.params
-                | {"username": self.username, "api_key": self.api_key},
+                params=params,
                 **kwargs,
             ) as response:
-                print(str(call))
-                response.raise_for_status()
                 await call.response.set(response)
 
-    async def send_sequential_calls(self):
-        """Executes job calls sequentially."""
-        if not self.dry_run and not self.thread:
-            print(f"... sequential - {self.wait}s wait")
-            async with aiohttp.ClientSession(timeout=self.timeout) as session:
-                for x in range(len(self.calls)):
-                    await self.send_call(self.calls[x], session, timeout=self.timeout)
-                    if x < len(self.calls) - 1:
-                        sleep(self.wait)
+        return str(call)
 
-    async def send_async_calls(self):
-        """Executes job calls asychronously."""
-        if not self.dry_run and self.thread:
-            print("... concurrent")
-            async with aiohttp.ClientSession(timeout=self.timeout) as session:
-                await asyncio.gather(
+    async def send_calls(self, get_kwargs: dict = {}, **kwargs):
+        """Executes a list of calls."""
+        if not self.dry_run:
+            self.wait_current = 0 - self.wait
+            if not self.thread:
+                print(f"... sequential - wait {self.wait}s")
+                connector = aiohttp.TCPConnector(limit_per_host=1)
+            else:
+                connector = aiohttp.TCPConnector()
+                print("... concurrent")
+            kwargs = dict(connector=connector, raise_for_status=True) | kwargs
+            async with aiohttp.ClientSession(**kwargs) as session:
+                res = await asyncio.gather(
                     *(
-                        self.send_call(call, session, timeout=self.timeout)
+                        self.send_call(call, session, **get_kwargs)
                         for call in self.calls
-                    )
+                    ),
+                    return_exceptions=True,
                 )
+                self.exceptions = [
+                    (res[x], self.calls[x])
+                    for x in range(len(self.calls))
+                    if not isinstance(res[x], str)
+                ]
 
-    def run(self):
+    def run(self, **kwargs):
         """Main function to execute a job."""
         t0 = perf_counter()
         self.cache_dir.mkdir(exist_ok=True)
@@ -178,9 +198,7 @@ class Job:
         self.assemble_calls()
         self.set_wait()
         self.clear_cache_func()
-        self.timeout = aiohttp.ClientTimeout(total=self.timeout)
-        asyncio.run(self.send_sequential_calls())
-        asyncio.run(self.send_async_calls())
+        asyncio.run(self.send_calls(**kwargs))
         self.dry_run_func()
         print(round(perf_counter() - t0, 4))
 
@@ -190,19 +208,19 @@ class Job:
             for k, v in self.__dict__.items()
             if k not in ["calls", "original_args"]
         }
-        calls = "____Calls____\n"
+        calls = "____Calls____ (up to 10 shown)\n"
         calls += "\n".join(
             [str(self.calls[x]) for x in range(len(self.calls)) if x < 10]
         )
         attrs = "____Attributes____\n"
-        attrs += "\n".join(
-            sorted(
-                [
-                    f'{(k)}    {(v if not k in ["username", "api_key"] else "*")}'
-                    for k, v in dt.items()
-                ]
-            )
-        )
+        for k, v in dt.items():
+            if k not in ["username", "api_key"]:
+                attrs += f"{(k)}    {str(v)[:min(len(str(v)),80)]}\n"
+            else:
+                if v:
+                    attrs += f"{(k)}    *\n"
+                else:
+                    attrs += f"{(k)}    None\n"
         return "\n".join([calls, attrs])
 
     def __init__(
@@ -211,15 +229,12 @@ class Job:
         cache_dir: str = "data",
         call_type: str | None = None,
         clear_cache: bool = False,
-        corpus: str | None = None,
         default_servers: dict = default_servers,
         dry_run: bool = False,
-        format: str = "json",
         infile: str | list | None = None,
         params: str | list | None = None,
         server: str = "local",
         thread: bool = False,
-        timeout: int = 30,
         username: str | None = None,
         wait_dict: dict = wait_dict,
     ) -> None:
@@ -228,15 +243,12 @@ class Job:
         self.cache_dir = cache_dir
         self.call_type = call_type
         self.clear_cache = clear_cache
-        self.corpus = corpus
         self.default_servers = default_servers
         self.dry_run = dry_run
-        self.format = format
         self.infile = infile
         self.params = params
         self.server = server
         self.thread = thread
-        self.timeout = timeout
         self.username = username
         self.wait_dict = wait_dict
         # additional args
@@ -263,13 +275,6 @@ class Job:
             if not isinstance(val, list):
                 setattr(self, x, [val])
         self.params = [util.parse_json_or_yaml(x) for x in self.params]
-        # numeric
-        for x in ["timeout"]:
-            val = getattr(self, x)
-            if isinstance(val, str) and "." in val:
-                setattr(self, x, float(val))
-            else:
-                setattr(self, x, int(val))
         # boolean
         for x in ["thread", "dry_run", "clear_cache"]:
             val = getattr(self, x)
@@ -312,13 +317,6 @@ def parse_args(args):
         help="clear cache directory (no arguments; ignored if `--dry-run`)",
     )
     parser.add_argument(
-        "-c",
-        "--corpus",
-        type=str,
-        default=os.environ.get("SGEX_CORPUS"),
-        help="corpus name (with path, if any: `preloaded/<corpus>`)",
-    )
-    parser.add_argument(
         "--default-servers",
         default=os.environ.get("SGEX_DEFAULT_SERVERS", default_servers),
         help="settings for default servers",
@@ -328,13 +326,6 @@ def parse_args(args):
         action="store_true",
         default=os.environ.get("SGEX_DRY_RUN", False),
         help="return assembled calls without sending (no arguments)",
-    )
-    parser.add_argument(
-        "-f",
-        "--format",
-        choices=["json", "xml", "xlsx", "csv", "txt"],
-        default=os.environ.get("SGEX_FORMAT", "json"),
-        help="API data output format",
     )
     parser.add_argument(
         "-i",
@@ -363,13 +354,6 @@ def parse_args(args):
         action="store_true",
         default=os.environ.get("SGEX_THREAD", False),
         help="run asynchronously, if allowed by server (no arguments)",
-    )
-    parser.add_argument(
-        "-z",
-        "--timeout",
-        type=int,
-        default=os.environ.get("SGEX_TIMEOUT", 30),
-        help="request timeout",
     )
     parser.add_argument(
         "-u",
